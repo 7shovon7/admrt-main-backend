f23a502e5abd0698546bdde39fadc1145bf840d1
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-08 04:07:24 +0600

    created all user models for profile data

--- admrt/settings.py
+++ admrt/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'core',
-    'space_host',
+    'users',
     # TODO: move corsheaders above core
     'corsheaders'
 ]
--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SpaceHostConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'space_host'
+    name = 'users'
