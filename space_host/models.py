eadae40fa42670b48896aaedc65752631158b237
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-08 02:50:06 +0600

    ongoing

--- space_host/models.py
+++ space_host/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from django.conf import settings
 from django.db import models
 
@@ -5,22 +6,48 @@ from django.db import models
 class SpaceHost(models.Model):
     profile_image = models.URLField(max_length=1024, null=True, blank=True)
     banner_image = models.URLField(max_length=1024, null=True, blank=True)
-    expertise = models.ManyToManyField('Expertise', null=True, blank=True)
+    topics = models.ManyToManyField('Topic', related_name='users')
+    intro = models.TextField(null=True, blank=True)
+    long_term_service_availability = models.CharField(max_length=100, null=True, blank=True)
+    languages = models.ManyToManyField('Language', related_name='users')
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f"{self.user.first_name} {self.user.last_name}"
+        return self.user.full_name
     
     class Meta:
-        ordering = ['user__first_name', 'user__last_name']
+        ordering = ['user__full_name']
 
 
-class Expertise(models.Model):
-    topic = models.CharField(max_length=100, unique=True)
+class Topic(models.Model):
+    # name = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100, unique=True)
 
     def __str__(self) -> str:
-        return str(self.topic)
+        return self.title
     
     class Meta:
-        ordering = ['topic']
-        
+        ordering = ['title']
+
+
+class Language(models.Model):
+    language = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.language
+    
+    class Meta:
+        ordering = ['language']
+
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    file_url = models.URLField(max_length=1024, null=True, blank=True)
+    youtube_url = models.URLField(max_length=255, null=True, blank=True)
+    user = models.ForeignKey(SpaceHost, on_delete=models.CASCADE, related_name='portfolios')
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
