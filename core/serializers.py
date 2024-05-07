3a0c66e2ebb45017c1136bf099e0bc1cb5edf0ac
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-07 23:40:14 +0600

    user role added

--- core/serializers.py
+++ core/serializers.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as Ba
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'user_role']
 
 
 class UserSerializer(BaseUserSerializer):
@@ -23,6 +23,7 @@ class TokenObtainPairSerializer(BaseTokenObtainPairSerializer):
         token['email'] = user.email
         token['first_name'] = user.first_name
         token['last_name'] = user.last_name
+        token['user_role'] = user.user_role
         return token
     
     def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
@@ -32,6 +33,7 @@ class TokenObtainPairSerializer(BaseTokenObtainPairSerializer):
             "username": self.user.username,
             "email": self.user.email,
             "first_name": self.user.first_name,
-            "last_name": self.user.last_name
+            "last_name": self.user.last_name,
+            "user_role": self.user.user_role
         }
         return data
