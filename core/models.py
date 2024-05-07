0a0ee578f05dfa8924448190da4f0ef0f020a92a
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-07 23:35:43 +0600

    user_role created

--- core/models.py
+++ core/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
+    SPACE_HOST = 'space_host'
+    ADVERTISER = 'advertiser'
+
+    ROLE_CHOICES = [
+        (SPACE_HOST, 'Space Host'),
+        (ADVERTISER, 'Advertiser')
+    ]
+
     email = models.EmailField(unique=True)
+    user_role = models.CharField(max_length=20, choices=ROLE_CHOICES, null=True, blank=True)
