8b0146f620a8b6d04a530bbaeba5491e3de6c53e
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-07 21:44:32 +0600

    another syntax changed

--- core/backends.py
+++ core/backends.py
@@ -1,4 +1,4 @@
-from typing import Any, Union, Optional
+from typing import Any, Union
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.base_user import AbstractBaseUser
@@ -6,7 +6,7 @@ from django.http import HttpRequest
 
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request: HttpRequest, username: Union[str, None], password: Union[str, None], **kwargs: Any) -> AbstractBaseUser | None:
+    def authenticate(self, request: HttpRequest, username: Union[str, None], password: Union[str, None], **kwargs: Any) -> Union[AbstractBaseUser, None]:
         UserModel = get_user_model()
         try:
             user = UserModel.objects.get(email=username)
