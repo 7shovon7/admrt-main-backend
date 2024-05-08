4f81f5b506eb18f7b5d24081062886d176f20536
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-08 23:45:14 +0600

    comment removed

--- users/views.py
+++ users/views.py
@@ -34,15 +34,6 @@ class UserViewSet(ListModelMixin, CreateModelMixin, RetrieveModelMixin, GenericV
             elif user_role == settings.K_ADVERTISER_ID:
                 serializer_class = AdvertiserSerializer
         return serializer_class
-    
-    # def create(self, request, *args, **kwargs):
-    #     data = request.data
-    #     data['user'] = request.user
-    #     serializer = self.get_serializer(data=data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def list(self, request):
         queryset = self.get_queryset()
