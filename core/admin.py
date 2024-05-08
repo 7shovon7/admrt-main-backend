d9d75e27fddc7a0174fdc045fc1244e2e38de271
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-08 16:45:03 +0600

    user params in admin panel fixed

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, _
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (_("Personal info"), {"fields": ("full_name",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "user_role",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
     add_fieldsets = (
         (
             None,
