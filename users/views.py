775bbd1a25ebf78d53d57c82951f25ffc49a5a4f
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-08 22:23:16 +0600

    permissions applied

--- users/views.py
+++ users/views.py
@@ -1,35 +1,42 @@
-from config import DEBUG
 from django.conf import settings
+from rest_framework import permissions
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework.response import Response
-from rest_framework import status
-# from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
-import traceback
+
 from core.serializers import UserSerializer
 from .models import SpaceHost, Advertiser
 from .serializers import SpaceHostSerializer, AdvertiserSerializer
 
 
-class UserViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
-    serializer_class = SpaceHostSerializer
+class UserViewSet(ListModelMixin, CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        user_role = self.request.user.user_role
+        user_id = self.request.user.id
+        if user_role is not None:
+            if user_role == settings.K_SPACE_HOST_ID:
+                queryset = SpaceHost.objects.filter(user=user_id).first()
+            elif user_role == settings.K_ADVERTISER_ID:
+                queryset = Advertiser.objects.filter(user=user_id).first()
+            else:
+                queryset = settings.AUTH_USER_MODEL.objects.filter(user_id=user_id).first()
+        else:
+            queryset = None
+        return queryset
+
+    def get_serializer_class(self):
+        serializer_class = UserSerializer
+        user_role = self.request.user.user_role
+        if user_role is not None:
+            if user_role == settings.K_SPACE_HOST_ID:
+                serializer_class = SpaceHostSerializer
+            elif user_role == settings.K_ADVERTISER_ID:
+                serializer_class = AdvertiserSerializer
+        return serializer_class
 
     def list(self, request):
-        try:
-            user_role = request.user.user_role
-            if user_role is None:
-                user = settings.AUTH_USER_MODEL.objects.filter(user_id=request.user.id).first()
-                serializer = UserSerializer
-            if user_role == settings.K_SPACE_HOST_ID:
-                user = SpaceHost.objects.filter(user=request.user.id).first()
-                serializer = SpaceHostSerializer
-            elif user_role == settings.K_ADVERTISER_ID:
-                user = Advertiser.objects.filter(user=request.user.id).first()
-                serializer = AdvertiserSerializer
-            else:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            return Response(serializer(user).data)
-        except AttributeError:
-            if DEBUG:
-                traceback.print_exc()
-            return Response(status=status.HTTP_401_UNAUTHORIZED, data={'details': "No user found"})
+        queryset = self.get_queryset()
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(queryset, many=False)
+        return Response(serializer.data)
