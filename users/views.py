ed32c3b642afc95478d507560b56d6ab900874aa
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-17 02:45:14 +0600

    get any user profile

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from rest_framework import permissions, status
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
@@ -184,34 +185,30 @@ class TopicViewSet(ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateM
 
 class UserViewSet(ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
     def get_queryset(self):
-        user_role = self.request.user.user_role
-        user_id = self.request.user.id
-        if user_role is not None:
-            if user_role == settings.K_SPACE_HOST_ID:
-                queryset = SpaceHost.objects.filter(user=user_id).first()
-            elif user_role == settings.K_ADVERTISER_ID:
-                queryset = Advertiser.objects.filter(user=user_id).first()
-            else:
-                queryset = settings.AUTH_USER_MODEL.objects.filter(user_id=user_id).first()
+        requested_user_id = self.request.GET.get('id')
+        if requested_user_id is not None:
+            user_id = requested_user_id
+        else:
+            user_id = self.request.user.id
+        # Fetch the user profile
+        queryset = get_user_model().objects.filter(id=user_id).first().profile
+        if hasattr(queryset, 'spacehost'):
+            self.serializer_class = SpaceHostSerializer
+            queryset = queryset.spacehost
+        elif hasattr(queryset, 'advertiser'):
+            self.serializer_class = AdvertiserSerializer
+            queryset = queryset.advertiser
         else:
             queryset = None
         return queryset
 
-    def get_serializer_class(self):
-        serializer_class = UserSerializer
-        user_role = self.request.user.user_role
-        if user_role is not None:
-            if user_role == settings.K_SPACE_HOST_ID:
-                serializer_class = SpaceHostSerializer
-            elif user_role == settings.K_ADVERTISER_ID:
-                serializer_class = AdvertiserSerializer
-        return serializer_class
-
     def list(self, request):
         queryset = self.get_queryset()
-        serializer_class = self.get_serializer_class()
-        serializer = serializer_class(queryset, many=False)
+        # serializer_class = self.get_serializer_class()
+        serializer = self.serializer_class(queryset, many=False)
         return Response(serializer.data)
     
     def create(self, request, *args, **kwargs):
