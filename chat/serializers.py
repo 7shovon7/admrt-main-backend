813a9153898fa47e018817543418b8fbb25d5f15
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-21 03:27:48 +0600

    full_name replaced by ids

--- chat/serializers.py
+++ chat/serializers.py
@@ -4,11 +4,11 @@ from .models import Conversation, Chat
 
 
 class ChatSerializer(serializers.ModelSerializer):
-    sender = serializers.CharField(source="sender.full_name", read_only=True)
-    receiver = serializers.CharField(source="receiver.full_name", read_only=True)
-    receiver_id = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all(), write_only=True, source='receiver')
+    sender_id = serializers.IntegerField(source="sender.id", read_only=True)
+    # receiver = serializers.CharField(source="receiver.full_name", read_only=True)
+    receiver_id = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all(), source='receiver')
     conversation = serializers.CharField(source='conversation.id', read_only=True)
 
     class Meta:
         model = Chat
-        fields = ["id", "sender", "receiver", "receiver_id", "conversation", "text", "delivered", "created_at"]
+        fields = ["id", "sender_id", "receiver_id", "conversation", "text", "delivered", "created_at"]
